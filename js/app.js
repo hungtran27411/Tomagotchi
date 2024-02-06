1cab8bf56f42d5947efc60cd8033b26b159d7550
Hung <hungtran27411@icloud.com>
2024-02-05 17:06:44 -0800

    Project Day one EOD update-Skeleton notes

---
+++ js/app.js
@@ -0,0 +1,27 @@
+
+//start with 100 %
+//die at 0%
+//primary math
+    //the game goal is to decrease value over time
+        // if i press a button to feed - reset the timer back to 1 minute and add 1 point
+        // If the button is not pressed decrease health, every 20 seconds if button is not pressed
+     // first phase lets test out timers
+     // https://www.shecodes.io/athena/52336-how-to-create-a-countdown-timer-in-javascript
+
+     //do something after 10 seond timeer
+    // function myFunction() {
+    //     console.log('Hello'); 
+    // }
+    // setTimeout(myFunction, 10000);
+//https://www.youtube.com/watch?v=x7WJEmxNlEs&t=301
+const myTimeout = setTimeout(myGreeting, 5000);
+
+
+function myGreeting() {
+  document.getElementById("demo").innerHTML = "Your 5 second timer has ran out!!"
+}
+
+function myStopFunction() {
+  clearTimeout(myTimeout);
+  myTimeout()
+}
