3c9f2a1f2006572f8b20aae0dddc0c889d1bdc86
Hung <hungtran27411@icloud.com>
2024-02-07 12:47:38 -0800

    Tomagotchi Day3, mid day push, updated CSS and added more functions

--- js/app.js
+++ js/app.js
@@ -1,58 +1,49 @@
+// Tamagotchi object
+const tomaEl = {
+  name: "Toma",
+  hunger: 50,
+  happiness: 50,
+  energy: 50,
+  feed: function() {
+    this.hunger -= 10;
+    this.happiness += 5;
+    this.updateDisplay();
+  },
+  play: function() {
+    this.hunger += 5;
+    this.happiness += 10;
+    this.energy -= 10;
+    this.updateDisplay();
+  },
+  rest: function() {
+    this.energy += 20;
+    this.hunger += 10;
+    this.updateDisplay();
+  },
+  updateDisplay: function() {
+    document.getElementById("name").innerText = this.name;
+    document.getElementById("hunger").innerText = this.hunger;
+    document.getElementById("happiness").innerText = this.happiness;
+    document.getElementById("energy").innerText = this.energy;
 
-//start with 100 %
-//die at 0%
-//primary math
-    //the game goal is to decrease value over time
-        // if i press a button to feed - reset the timer back to 1 minute and add 1 point
-        // If the button is not pressed decrease health, every 20 seconds if button is not pressed
-     // first phase lets test out timers
-     // https://www.shecodes.io/athena/52336-how-to-create-a-countdown-timer-in-javascript
+    // Update overall health
+    const health = Math.round((this.hunger + this.happiness + this.energy) / 3);
+    document.getElementById("health-bar-fill").style.width = health + "%";
+  }
+};
 
-     //do something after 10 seond timeer
-    // function myFunction() {
-    //     console.log('Hello'); 
-    // }
-    // setTimeout(myFunction, 10000);
-//https://www.youtube.com/watch?v=x7WJEmxNlEs&t=301
-// const myTimeout = setTimeout(myGreeting, 5000);
+// Event listeners for buttons
+document.getElementById("Feed-Button").addEventListener("click", () => {
+  tomaEl.feed();
+});
 
+document.getElementById("Play-Button").addEventListener("click", () => {
+  tomaEl.play();
+});
 
-// function myGreeting() {
-//   document.getElementById("demo").innerHTML = "Your 5 second timer has ran out!!"
-// }
+document.getElementById("Rest-Button").addEventListener("click", () => {
+  tomaEl.rest();
+});
 
-// function myStopFunction() {
-//   clearTimeout(myTimeout);
-//   myTimeout()
-// }
-
-
-// let start = document.querySelector('#start');
-// let stop  = document.querySelector('#stop');
-
-// start.addEventListener('click', function() {
-// 	let timerId = setInterval(function() {
-// 		console.log('!')
-// 	}, 1000);
-// });
-
-// // Stopping the timer:
-// stop.addEventListener('click', function() {
-// 	clearInterval(timerId);
-// });
-//The greater than or equal ( >= ) operator returns true if the left operand is greater than or equal to the right operand, and false otherwise.
-
- // Decrease progress bar width gradually over 10 seconds
- let animation; // Variable to store animation ID
-
-  // Function to start the progress bar animation
-  document.getElementById('startButton').addEventListener('click', () => {
-    const progressBarFill = document.getElementById('progressBar');
-    progressBarFill.style.animationPlayState = 'running'; // Start the animation
-  });
-
-  // Function to stop the progress bar animation
-  document.getElementById('stopButton').addEventListener('click', () => {
-    const progressBarFill = document.getElementById('progressBar');
-    progressBarFill.style.animationPlayState = 'paused'; // Pause the animation
-  });
+// Initial display
+tomaEl.updateDisplay();
