a9cd70af013fd7c600a1bb29c1b5dba52d6499fd
Hung <hungtran27411@icloud.com>
2024-02-09 02:56:27 -0800

    Tomagatchi day 4, final

--- README.md
+++ README.md
@@ -3,28 +3,33 @@
 Tomagotchi
 
         OverView
-                HTML, CSS, and JavaScript to interact with our pet.
+                HTML, CSS, and JavaScript to interact with our pet. Tamagotchis are a small alien species that deposited an egg on Earth to see what life was like, and it is up to the player to raise the egg into an adult creature.
 
-                    Tomagotchi's will age (increase) health level will decrease if 3 thresholds are not maintained.
-                    combined levels gets too high or too low
-                    maybe - Use BMI as a way to calculate measure total health.(Score)
+                    My interpretation : How healthy can you keep pet Toma in 10 seconds! It is based on highest score so you can pass the digital key around and see who can get the highest score or healthy habits.
 
-                    *Exercise
-                        decreased value if not clicked
-                    *Food
-                        decreased value if not clicked
-                        if too much food is given  total health should go down
-                        if not enough food and too much activity health will decrease
-                    *Rest
-                        decrease value if not clicked
-                        *
+                    Feed Button: Will Increase Toma's weight
+                                 Will decrease Toma's energy
+                                 Will increase Toma's happiness
+
+                    Play Button: Will Decrease Toma's weight
+                                 Will Increase Toma's energy
+                                 Will increase Toma's happiness
+
+                    Rest Button  Will Increase Toma's weight
+                                 Will Increase Toma's energy
+                                 Will increase Toma's happiness
+
+                The above will be accomplished using :
+
+                HTML to create the three buttons, feed, play and rest:
+
+                <div id="buttons">  to create the section/group  or division of buttons.
+                     creates the buttons
+                        <button id="feedButton">Feed</button>
+                         feedButton is the id of the button, i will use this as the identifier in javascript and CSS
+
+
 
         Appearance
-                    a pikachoo image with multiple horizontal bar graph
-                    image of the animation will change based on overhealth and age
-
-
-                    three main buttons buttons
-                            * Feed
-                            * Play
-                            * Rest
+                    An Android Image
+                    Progress bar or timout bar
--- js/app.js
+++ js/app.js
@@ -1,49 +1,74 @@
-// Tamagotchi object
 const tomaEl = {
-  name: "Toma",
-  hunger: 50,
-  happiness: 50,
-  energy: 50,
-  feed: function() {
-    this.hunger -= 10;
-    this.happiness += 5;
-    this.updateDisplay();
+  name: "Toma!", // String
+  weight: 0, // Number
+  happiness: 0, //Number
+  energy: 0,  //Number
+  timer: 10, // Number
+  buttonsEnabled: true, //Boolean // enable buttons when we start
+  feedButton: function() {  // when the feed button is pressed, this will change the value
+    if (this.buttonsEnabled) {     // this function will not work if the time has ran out.   
+      this.weight += 10;
+      this.happiness += 1;
+      this.energy -= 10;
+      this.updateDisplay();// this will paint the screen with the new information.
+    }
   },
-  play: function() {
-    this.hunger += 5;
-    this.happiness += 10;
-    this.energy -= 10;
-    this.updateDisplay();
+  playButton: function() {
+    if (this.buttonsEnabled) {
+      this.weight -= 10;
+      this.happiness += 10;
+      this.energy += 10;
+      this.updateDisplay();
+    }
   },
-  rest: function() {
-    this.energy += 20;
-    this.hunger += 10;
-    this.updateDisplay();
-  },
+  restButton: function() {
+    if (this.buttonsEnabled) {
+      this.energy += 2;
+      this.weight += 1;
+      this.happiness += 5;
+      this.updateDisplay();
+    }
+  }, 
+
   updateDisplay: function() {
-    document.getElementById("name").innerText = this.name;
-    document.getElementById("hunger").innerText = this.hunger;
-    document.getElementById("happiness").innerText = this.happiness;
-    document.getElementById("energy").innerText = this.energy;
+    document.getElementById("name").innerText = this.name;//paint to screen the name
+    document.getElementById("weight").innerText = this.weight;//paint to screen the weight
+    document.getElementById("happiness").innerText = this.happiness;//paint to screen the happy
+    document.getElementById("energy").innerText = this.energy;//paint to screen the energy
+    document.getElementById("timer").innerText = this.timer; //paint to screen the what is  left on time
+  
+    // Update the score
+    const total = this.weight + this.happiness + this.energy;
+    document.getElementById("finalScore").innerText = total;
 
-    // Update overall health
-    const health = Math.round((this.hunger + this.happiness + this.energy) / 3);
-    document.getElementById("health-bar-fill").style.width = health + "%";
+    // if the timer is less than or equal 0 disable the buttons
+    if (this.timer <= 0) {
+      this.buttonsEnabled = false;
+    }
   }
-};
+}; // Closing tomaEl object
 
-// Event listeners for buttons
-document.getElementById("Feed-Button").addEventListener("click", () => {
-  tomaEl.feed();
-});
+// Timer Function 
+function startTimer(durationInSeconds) {
+  let timer = durationInSeconds;
+  const intervalId = setInterval(() => {
+    tomaEl.timer = timer;     
+    if (--timer < 0) { // if we ran out of time then disable 
+      clearInterval(intervalId);
+      tomaEl.buttonsEnabled = false; // if we ran out of time then disable 
+    }
+    tomaEl.updateDisplay(); // paint the screen every second
+  }, 1000); //10,000 milliseconds is 10 seconds, the game will only run for 10 minutes
+}
 
-document.getElementById("Play-Button").addEventListener("click", () => {
-  tomaEl.play();
-});
+// Event listener for the button.. 
+// i had to use bind for the buttons to work, google and trial and error
+document.getElementById("feedButton").addEventListener("click", tomaEl.feedButton.bind(tomaEl));
+document.getElementById("playButton").addEventListener("click", tomaEl.playButton.bind(tomaEl));
+document.getElementById("restButton").addEventListener("click", tomaEl.restButton.bind(tomaEl));
 
-document.getElementById("Rest-Button").addEventListener("click", () => {
-  tomaEl.rest();
-});
+// Start the timer, this initiates the funtion startTimer
+startTimer(tomaEl.timer);
 
 // Initial display
 tomaEl.updateDisplay();
