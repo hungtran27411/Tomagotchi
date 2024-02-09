3fb305dd83491e3e772139c546ea2c6094476e44
Hung <hungtran27411@icloud.com>
2024-02-09 10:05:44 -0800

    fix

--- testfile.js
+++
@@ -1,5 +0,0 @@
-const healthBarFill = document.getElementById("health-bar-fill");
-  healthBarFill.style.animationDuration = "20s"; 
-  healthBarFill.classList.remove("paused");
-  void healthBarFill.offsetWidth; // Trigger reflow to restart animation
-  healthBarFill.classList.add("paused");
